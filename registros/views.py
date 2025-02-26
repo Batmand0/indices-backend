4241a2e7d1a46cd1be2237d5693425cdd4946fe0
Armando <mexgamer56@gmail.com>
2025-02-26 10:23:10 -0800

    Comentarios en views de carpeta registros

--- registros/views.py
+++ registros/views.py
@@ -64,9 +64,18 @@ class IngresoUpload(views.APIView):
         return data
 
     def post(self, request, filename, format=None):
-        ESTRUCTURA = [(r'^curp$', 'CURP'), (r'^no_control$', 'NO_CONTROL'), (r'^paterno$', 'PATERNO'), (r'^materno$', 'MATERNO'), (r'^nombre$', 'NOMBRE'), (r'^carrera$', 'CARRERA'), (r'^[12][0-9]{3}[13]$', 'NUMERO DE PERIODO')]
+        ESTRUCTURA = [(r'^curp$', 'CURP'), (r'^no_control$', 'NO_CONTROL'), 
+                     (r'^paterno$', 'PATERNO'), (r'^materno$', 'MATERNO'), 
+                     (r'^nombre$', 'NOMBRE'), (r'^carrera$', 'CARRERA'), 
+                     (r'^[12][0-9]{3}[13]$', 'NUMERO DE PERIODO')]
+        # Obtiene el archivo enviado en la solicitud HTTP
         file_obj = request.data['file']
+
+        # Carga el archivo Excel en un objeto Workbook de openpyxl, 
+        # con data_only=True para obtener los valores calculados en lugar de las fórmulas
         wb = openpyxl.load_workbook(file_obj, data_only=True)
+
+        # Selecciona la hoja activa del archivo Excel
         ws = wb.active
 
         results = {"errors": [], "created": 0}
@@ -79,6 +88,7 @@ class IngresoUpload(views.APIView):
             if match is None:
                 return Response(status=400, data={'message': f'Se esperaba el campo {expresion[1]} pero se obtuvo {header_row[i].value}'})
 
+        
         for row in ws.iter_rows(min_row=2):
             try:
                 # se debe verificar que todos los campos tengan datos
@@ -100,27 +110,37 @@ class IngresoUpload(views.APIView):
                 )
                 alumno = None
                 try:
+                    # Intentar obtener el alumno con el número de control proporcionado en los datos
                     alumno = Alumno.objects.get(no_control=data['no_control'])
+                    # Verificar si la carrera del plan del alumno coincide con la carrera proporcionada en los datos
                     if alumno.plan.carrera.clave != data['carrera']:
+                        # Si no coinciden, agregar un error a los resultados y continuar con la siguiente iteración
                         results['errors'].append({'type': 'Carrera', 'message': f"La carrera {data['carrera']} no concuerda con el plan registrado {alumno.plan.clave}", 'row_index': row[0].row})
                         continue
                 except Alumno.DoesNotExist:
+                    # Si el alumno no existe, buscar el último plan asociado a la carrera proporcionada
                     plan = Plan.objects.filter(carrera=carrera).last()
+                    # Crear un nuevo alumno con los datos proporcionados
                     alumno = Alumno.objects.create(
                         no_control=data['no_control'],
                         curp=personal,
                         plan=plan
                     )
 
-                # CREAR REGISTRO DE INGRESO
+                # Crear un registro de ingreso si no existe uno con el mismo alumno, periodo y tipo
                 if not Ingreso.objects.filter(alumno=alumno, periodo=str(header_row[6].value), tipo=data['tipo']).exists():
                     ingreso = Ingreso(alumno=alumno, periodo=str(header_row[6].value), tipo=data['tipo'])
                     try:
+                        # Calcular el número de semestre del ingreso
                         ingreso.calcular_num_semestre()
+                        # Validar el ingreso
                         ingreso.full_clean()
+                        # Guardar el ingreso en la base de datos
                         ingreso.save()
+                        # Incrementar el contador de registros creados
                         results['created'] += 1
                     except Exception as ex:
+                        # Manejar errores de validación y otros errores
                         if ex.message_dict:
                             for err in ex.message_dict:
                                 results['errors'].append({'type': err, 'message': ex.message_dict[err], 'row_index': row[0].row})
@@ -129,11 +149,15 @@ class IngresoUpload(views.APIView):
                         continue
 
             except Carrera.DoesNotExist as ex:
+                # Manejar el caso en que la carrera no exista
                 results['errors'].append({'type': str(type(ex)), 'message': 'La carrera indicada no existe', 'row_index': row[0].row})
                 continue
             except Exception as ex:
+                # Manejar otros errores generales
                 results['errors'].append({'type': str(type(ex)), 'message': str(ex), 'row_index': row[0].row})
                 continue
+
+        # Devolver una respuesta con el estado 200 y los resultados
         return Response(status=200, data=results)
 
 ### EGRESO
@@ -169,8 +193,14 @@ class EgresoUpload(views.APIView):
 
     def post(self, request, filename, format=None):
         ESTRUCTURA = [(r'^no_control$', 'NO_CONTROL'), (r'^[12][0-9]{3}[13]$', 'PERIODO')]
+        # Obtiene el archivo enviado en la solicitud HTTP
         file_obj = request.data['file']
+
+        # Carga el archivo Excel en un objeto Workbook de openpyxl, 
+        # con data_only=True para obtener los valores calculados en lugar de las fórmulas
         wb = openpyxl.load_workbook(file_obj, data_only=True)
+
+        # Selecciona la hoja activa del archivo Excel
         ws = wb.active
 
         results = {"errors": [], "created": 0}
@@ -188,7 +218,9 @@ class EgresoUpload(views.APIView):
                 data = self.to_dict(row)
                 if data is None:
                     continue
+                # Buscar el alumno con el número de control proporcionado en los datos
                 alumno = Alumno.objects.get(pk=data['no_control'])
+                # Se crea un registro de egreso con el alumno y el periodo proporcionados
                 egresado = Egreso.objects.create(periodo=str(header_row[1].value), alumno=alumno)
                 egresado.save()
                 results['created'] += 1
@@ -234,8 +266,14 @@ class TitulacionUpload(views.APIView):
 
     def post(self, request, filename, format=None):
         ESTRUCTURA = [(r'^no_control$', 'NO_CONTROL'), (r'^[12][0-9]{3}[13]$', 'NUMERO DE PERIODO')]
+        # Obtiene el archivo enviado en la solicitud HTTP
         file_obj = request.data['file']
+
+        # Carga el archivo Excel en un objeto Workbook de openpyxl, 
+        # con data_only=True para obtener los valores calculados en lugar de las fórmulas
         wb = openpyxl.load_workbook(file_obj, data_only=True)
+
+        # Selecciona la hoja activa del archivo Excel
         ws = wb.active
 
         results = {"errors": [], "created": 0}
@@ -296,8 +334,14 @@ class LiberacionInglesUpload(views.APIView):
 
     def post(self, request, filename, format=None):
         ESTRUCTURA = [(r'^no_control$', 'NO_CONTROL'), (r'^[12][0-9]{3}[13]$', 'NUMERO DE PERIODO')]
+        # Obtiene el archivo enviado en la solicitud HTTP
         file_obj = request.data['file']
+
+        # Carga el archivo Excel en un objeto Workbook de openpyxl, 
+        # con data_only=True para obtener los valores calculados en lugar de las fórmulas
         wb = openpyxl.load_workbook(file_obj, data_only=True)
+
+        # Selecciona la hoja activa del archivo Excel
         ws = wb.active
 
         results = {"errors": [], "created": 0}
@@ -330,7 +374,9 @@ class LiberacionInglesUpload(views.APIView):
 @permission_classes([IsAuthenticated&IsAdminUser])
 def corte(request):
     periodo = getPeriodoActual()
+    # Verificar si no existen registros que pertenezcan a un corte para el periodo actual
     if not Ingreso.objects.contiene_corte(periodo) and not Egreso.objects.contiene_corte(periodo) and not Titulacion.objects.contiene_corte(periodo) and not LiberacionIngles.objects.contiene_corte(periodo):
+        # Realizar corte
         ingresos = Ingreso.objects.realizar_corte(periodo)
         egresos = Egreso.objects.realizar_corte(periodo)
         titulaciones = Titulacion.objects.realizar_corte(periodo)
@@ -339,29 +385,48 @@ def corte(request):
     else:
         return Response(status=400, data={'periodo': periodo ,'message': f'No se puede realizar un corte ya que existen registros que pertenecen a un corte para el periodo {periodo}.'})
 
-# Lee un renglon de excel y lo convierte a un diccionario de acuerdo a los campos del header_row
+# Esta función convierte una fila de Excel en un diccionario basado en los encabezados
 def row_to_dict(header_row, data_row):
-    clean_header = clean_row(header_row)
-    clean_data = clean_row(data_row)
+    # Limpia las filas eliminando celdas vacías
+    clean_header = clean_row(header_row)  # Limpia la fila de encabezados
+    clean_data = clean_row(data_row)      # Limpia la fila de datos
+    
+    # Lista de campos esperados en el archivo Excel
     keywords = ['curp', 'no_control', 'paterno', 'materno', 'nombre', 'carrera']
+    
+    # Inicializa el diccionario con una lista vacía para los periodos
     row_dict = {'periodos': []}
+    
+    # Procesa cada celda en la fila de datos
     for cell in clean_data:
+        # Obtiene el índice de la columna (restando 1 porque Excel empieza en 1)
         index = cell.column - 1
+        # Convierte el encabezado a minúsculas
         header = str(clean_header[index].value).lower()
+        # Obtiene el valor de la celda, si es None retorna None
         value = str(cell.value) if cell.value else None
+        
+        # Si el encabezado es uno de los campos esperados
         if header in keywords:
             row_dict[header] = value
+        # Si el encabezado es un periodo (formato: YYYYS donde Y=año, S=semestre)
         elif re.match(r'^[12][0-9]{3}[13]$', header):
+            # Si hay un valor, agrega una tupla (periodo, tipo) a la lista de periodos
             if value: row_dict['periodos'].append((header, value[0:2]))
         else:
+            # Si el encabezado no es reconocido, lanza una excepción
             raise Exception(f'Campo "{header}" no es reconocido')
-    # ordenar periodos por fecha
+    
+    # Ordena los periodos cronológicamente
     row_dict['periodos'].sort(key=lambda x: x[0])
     return row_dict
 
+# Esta función limpia una fila de Excel eliminando las celdas vacías
 def clean_row(row):
     clean = []
+    # Recorre cada celda en la fila
     for cell in row:
+        # Si la celda tiene un valor, la agrega a la lista limpia
         if cell.value is not None:
             clean.append(cell)
     return clean
